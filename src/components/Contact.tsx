e821ccadb726dbea6756405617d23be9eab635d8
Biplop Maharjan <maharjankija@gmail.com>
2026-06-15 13:47:45 +0545

    feat: add Google Maps link to address

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -13,7 +13,10 @@ export default function Contact() {
             <div className="cd">
               <div className="cd-icon">📍</div>
               <div className="cd-text">
-                <strong>Address</strong>M7W2+77J Chundevi Repair Road, Chandragiri 44600
+                <strong>Address</strong>
+                <a href="https://www.google.com/maps/place/Active+Fitness/@27.6956757,85.2505518,3a,75y,90t/data=!3m8!1e2!3m6!1sCIHM0ogKEICAgICk-bnGAQ!2e10!3e12!6shttps:%2F%2Flh3.googleusercontent.com%2Fgps-cs-s%2FAPNQkAGiLD5PNpYtsTILVDbIcOeaV77gv7kXNDm6XIMGHOTSckzDCY5yqKNLoVIgaIwp8BDfZk5ME0DI-Zjh7M-TsCexvA23sxuQ_DPr8iZs8-weZU_FYWVfUpK-a_P2qvrrd0Cp9-A%3Dw203-h135-k-no!7i6000!8i4000!4m10!1m2!2m1!1sactive+fitness+gym!3m6!1s0x39eb23f49882a999:0xf6f9de82afde6aa!8m2!3d27.6957106!4d85.2506291!15sChJhY3RpdmUgZml0bmVzcyBneW1aFCISYWN0aXZlIGZpdG5lc3MgZ3ltkgEPZ3ltbmFzdGljc19jbHVimgEjQ2haRFNVaE5NRzluUzBWSlEwRm5TVVJFTW05SGFsWm5FQUXgAQD6AQQIABAj!16s%2Fg%2F11h110ryvp?entry=ttu&g_ep=EgoyMDI2MDYxMC4wIKXMDSoASAFQAw%3D%3D" target="_blank" rel="noopener noreferrer" style="color: var(--light); text-decoration: underline; text-underline-offset: 3px;">
+                  M7W2+77J Chundevi Repair Road, Chandragiri 44600
+                </a>
               </div>
             </div>
             <div className="cd">
